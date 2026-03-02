fa974e70d6b50be44a139ae6f956b53c9d74f4de
Abraham <sanchezbaj@gmail.com>
2026-03-02 10:26:29 -0600

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abrahamjsb.github.io',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
